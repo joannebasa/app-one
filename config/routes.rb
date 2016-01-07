1f413b53b0c279dad7b1833994f8d45381c59c83
“PJ <YOUR@EMAIL.com>
2016-01-07 15:54:12 +0000

    fixed links

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,8 @@ Rails.application.routes.draw do
   get 'static_pages/products'
 
   get 'static_pages/faqs'
-
-  get 'static_pages/index'
+  
+  get 'static_pages/index' 
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
